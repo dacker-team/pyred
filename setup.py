65930b1a5d4603e4a0d6dbcf60cb01ce2fc2ddda
Pierre-François Lucet <pierre-francois@dacker.co>
2018-02-23 18:07:25 +0100

    next version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='pyred',
-    version='0.0.5',
+    version='0.0.6',
     description='Easily send data to Amazon Redshift',
     long_description=readme,
     author='Dacker',
