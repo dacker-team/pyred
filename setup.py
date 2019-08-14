478c6dc25728119ad9e25664b0d8d38b3f6d0d11
Vincent Monet <vincent@dacker.co>
2019-08-14 11:22:49 +0200

    doc

--- setup.py
+++ setup.py
@@ -13,9 +13,6 @@ reqs = [str(ir.req) for ir in reqs]
 with open('README.rst') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='pyred',
     version='0.1.8',
@@ -24,9 +21,13 @@ setup(
     author='Dacker',
     author_email='hello@dacker.co',
     url='https://github.com/dacker-team/pyred',
-    license=license,
     keywords='send data amazon redshift easy',
     packages=find_packages(exclude=('tests', 'docs')),
     python_requires='>=3',
     install_requires=reqs,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
