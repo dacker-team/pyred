6ff4edf06db9d515056886458a7f954116a1e339
Adrien Moy <adrien@dacker.co>
2020-05-12 09:42:30 +0200

    fix

--- pyred/RedDBStream.py
+++ pyred/RedDBStream.py
@@ -81,15 +81,8 @@ class RedDBStream(dbstream.DBStream):
             final_data = []
             for x in temp_row:
                 for y in x:
-                    if isinstance(y, dict):
-                        y = list(y.values())
-                    if isinstance(y, list):
-                        if len(y) == 0:
-                            y = None
-                        elif len(y) == 1:
-                            y = y[0]
-                        else:
-                            y = ', '.join(y)
+                    if isinstance(y, (list, dict)):
+                        y = json.dumps(y)
                     final_data.append(y)
 
             temp_string = ','.join(map(lambda a: '(' + ','.join(map(lambda b: '%s', a)) + ')', tuple(temp_row)))
