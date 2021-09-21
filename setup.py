2adad3a997f6e285c65dd61a411106fef54e0523
Pierre-François Lucet <pierre-francois@dacker.co>
2021-09-21 18:37:47 +0200

    pyred next version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='pyred',
-    version='0.3.46',
+    version='0.3.47',
     description='Easily send data to Amazon Redshift',
     long_description=readme,
     author='Dacker',
